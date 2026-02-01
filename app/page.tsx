c340d384e7c5e669974cd3644c15c43f6e0bea86
Sam Stenner <sam.stenner2@gmail.com>
2026-01-31 21:26:14 -0800

    chore: footer

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,15 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-[#f5f5f5]">
       <Profile />
-      <div className="flex gap-2 items-center justify-center py-4">
-        <Link className="text-muted-foreground hover:text-primary text-xs" href="/privacy">Privacy Policy</Link>
-        <span className="text-muted-foreground text-xs">|</span>
-        <Link className="text-muted-foreground hover:text-primary text-xs" href="/tos">Terms of Service</Link>
-      </div>
+      <footer className="flex flex-col items-center justify-center pb-4 pt-20">
+        <span className="text-2xl font-bold text-primary">Unhinged</span>
+        <span className="text-muted-foreground font-medium text-xs mt-1">Generate photos for your dating profile</span>
+        <div className="flex gap-2 items-center justify-center py-4">
+          <Link className="text-muted-foreground hover:text-primary text-xs" href="/privacy">Privacy Policy</Link>
+          <span className="text-muted-foreground text-xs">|</span>
+          <Link className="text-muted-foreground hover:text-primary text-xs" href="/tos">Terms of Service</Link>
+        </div>
+      </footer>
     </div>
   );
 }
