c0ebaabe3fee6fd3e8fc6bba725dfe9a72ec03bc
Sam Stenner <sam.stenner2@gmail.com>
2026-01-31 21:17:11 -0800

    fix: privacy

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import Profile from "@/components/Profile";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#f5f5f5]">
       <Profile />
+      <div className="flex gap-2 items-center justify-center py-4">
+        <Link className="text-muted-foreground hover:text-primary text-xs" href="/privacy">Privacy Policy</Link>
+        <span className="text-muted-foreground text-xs">|</span>
+        <Link className="text-muted-foreground hover:text-primary text-xs" href="/tos">Terms of Service</Link>
+      </div>
     </div>
   );
 }
